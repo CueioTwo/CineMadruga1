b69906deef8d4a81b2ea3426ebf0b7c2385fc078
= <=>
2023-11-26 15:01:59 -0300

    Quase

--- php/salvar_filme.php
+++ php/salvar_filme.php
@@ -1,17 +1,118 @@
 <?php
 include "conect.php";
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
+    $nome_filme = $_POST['nome_filme'];
+    $diretor = $_POST['diretor'];
+    $duracao = $_POST['duracao'];
+    $genero = $_POST['genero'];
+    $datalanca = $_POST['datalanca'];
+    $sinopse = $_POST['sinopse'];
+    $classificacao = $_POST['classificacao'];
 
-$nome_filme = $_POST['nome_filme'];
-$diretor = $_POST['diretor'];
-$duracao = $_POST['duracao'];
-$genero = $_POST['genero'];
-$datalanca = $_POST['datalanca'];
-$sinopse = $_POST['sinopse'];
-$classificacao = $_POST['classificacao'];
+    $dados2 = $conn -> query("SELECT * FROM filme WHERE nome_filme = '$nome_filme' AND diretor = '$diretor' AND datalanca = '$datalanca'");
+    $check = mysqli_num_rows($dados2);
 
-$conn -> query("INSERT INTO filme(id_filme, nome_filme, diretor, duracao, genero, datalanca, sinopse, class_indicativa)
-VALUES(NULL, '$nome_filme', '$diretor', '$duracao',  '$genero', '$datalanca','$sinopse', '$classificacao')");
+    if($check < 1){
 
-header('Location: ../indexo.html');
+        if($nome_filme != "" &&  $diretor != "" && $duracao != "" && $genero != "" && $datalanca != "" && $sinopse = "" && $classificacao = ""){
+
+            $conn -> query("INSERT INTO filme(id_filme, nome_filme, diretor, duracao, genero, datalanca, sinopse, class_indicativa)
+            VALUES(NULL, '$nome_filme', '$diretor', '$duracao',  '$genero', '$datalanca','$sinopse', '$classificacao')");
+            echo "<script language='javascript'>";
+            echo "alert('Cadastrado com suscesso');";
+            echo 'window.location.assign("../indexo.html");';
+            echo "</script>";
+
+        }else{
+
+            echo "<script language='javascript'>";
+            echo "alert('Dados faltando');";
+            echo "</script>";
+        }
+        
+    }else{
+
+        echo "<script language='javascript'>";
+        echo "alert('Filme já cadastrado');";
+        echo "</script>";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CineMagruga</title>
+    <link rel="stylesheet" href="../css/cadfilme_2.css">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <div class="imagem">
+            <img src="../img/pipoca.png" alt="" id="Img">
+        </div>
+        <div class="coisas">
+            <form action="../php/salvar_filme.php" method="post" enctype="multipart/form-data">
+                <div class="titulo"><h1>Cadastro de Filme</h1></div>
+                <div class="inputs">
+                    <div class="metade1">
+                        <div class="input">
+                            <label for="nome_filme">Nome do Filme: </label> <br>
+                            <input type="text" name="nome_filme" id="nome"  max="50">
+                        </div>
+                        <div class="input">
+                            <label for="diretor">Diretor: </label> <br>
+                            <input type="text" name="diretor" id="diretor" max="20" >
+                        </div>
+                        <div class="input">
+                            <label for="duracao">Duranção: </label> <br>
+                            <input type="time" id="duracao" max="04:00" name="duracao">
+                        </div>
+                        
+                        <div class="input">
+                            <label for="genero">Gênero: </label> <br>
+                            <select name="genero" id="genero">
+                                <option value="acao">Ação</option>
+                                <option value="terror">Terror</option>
+                                <option value="comedia">Comédia</option>
+                                <option value="Suspense">Suspense</option>
+                                <option value="Romance">Romance</option>
+                            </select>
+                        </div>
+                        <div class="input">
+                            <label for="datalanca">Data de Lançamento: </label> <br>
+                            <input type="date" name="datalanca" id="datanasc" min="1920-01-01">
+                        </div>
+                    </div>
+                    <div class="metade2">
+                        
+                        <div class="input">
+                            <label for="sinopse">Sinopse: </label> <br>
+                            <textarea name="sinopse" id="" cols="40" rows="10"></textarea>
+                        </div>
+                        <div class="input">
+                            <label for="classificacao">Classificação Indicativa: </label> <br>
+                            <select name="classificacao" id="classificacao">
+                                <option value="livre">Livre</option>
+                                <option value="10 anos">10 anos</option>
+                                <option value="14 anos">14 anos</option>
+                                <option value="16 anos">16 anos</option>
+                                <option value="18 anos">18 anos</option>
+                            </select>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                <div class="botoes">
+                    <input type="submit" id="enviar" value="Enviar"> <br>
+                    <input type="reset" value="Limpar">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- php/salvar_usuario.php
+++ php/salvar_usuario.php
@@ -1,21 +1,117 @@
 <?php
 include "conect.php";
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $repemail = $_POST['rep_email'];
+    $senha = $_POST['senha'];
+    $repsenha = $_POST['rep_senha'];
+    $cep = $_POST['cep'];
+    $apelido = $_POST['apelido'];
+    $data_nasc = $_POST['datanasc'];
+    $tipo = 0;
+    
+
+    $dados2 = $conn -> query("SELECT * FROM usuario WHERE email = '$email'");
+    $check = mysqli_num_rows($dados2);
+
+    if($check < 1){
+        if($email == $repemail &&  $senha == $repsenha){
+            $conn -> query("INSERT INTO usuario(id, nome, apelido, email, senha, cep, datanasc, tipo)
+            VALUES(NULL, '$nome', '$apelido', '$email',  '$senha', '$cep', '$data_nasc', '$tipo')");
+            echo "<script language='javascript'>";
+            echo "alert('Cadastrado com suscesso');";
+            echo 'window.location.assign("../indexo.html");';
+            echo "</script>";
+
+        }else{
+            echo "<script language='javascript'>";
+            echo "alert('O emails ou senha não é iguais a sua repetições');";
+            echo "</script>";
+        }
+        
+        
+
+        
+    }else{
+        echo "<script language='javascript'>";
+        echo "alert('Email já cadastrado');";
+        echo "</script>";
+    }
+
+}
 
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$repemail = $_POST['rep_email'];
-$senha = $_POST['senha'];
-$repsenha = $_POST['rep_senha'];
-$cep = $_POST['cep'];
-$apelido = $_POST['apelido'];
-$data_nasc = $_POST['datanasc'];
-$tipo = 0;
 
-
-
-$conn -> query("INSERT INTO usuario(id, nome, apelido, email, senha, cep, datanasc, tipo)
-VALUES(NULL, '$nome', '$apelido', '$email',  '$senha', '$cep', '$data_nasc', '$tipo')");
-
-header('Location: ../indexo.html');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CineMagruga</title>
+    <link rel="stylesheet" href="../css/cadastro_2.css">
+    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <div class="container">
+        <div class="imagem">
+            <img src="../img/pipoca.png" alt="" id="Img">
+        </div>
+        <div class="coisas">
+            <form action="../php/salvar_usuario.php" method="post" enctype="multipart/form-data">
+                <div class="titulo"><h1>Cadastro</h1></div>
+                <div class="inputs">
+                    <div class="metade1">
+                        <div class="input">
+                            <label for="nome">Nome: </label> <br>
+                            <input type="text" name="nome" id="nome"  max="50">
+                        </div>
+                        <div class="input">
+                            <label for="apelido">Apelido: </label> <br>
+                            <input type="text" name="apelido" id="apelido" max="20" >
+                        </div>
+                        <div class="input">
+                            <label for="email">Email: </label> <br>
+                            <input type="email" name="email" id="email" max="60" >
+                        </div>
+                        <div class="input">
+                            <label for="rep_email">Repita o email: </label> <br>
+                            <input type="email" name="rep_email" id="rep_email" max="60" >
+                            
+                        </div>
+                    </div>
+                    <div class="metade2">
+                        <div class="input">
+                            <label for="cep">CEP: </label> <br>
+                            <input type="text" name="cep" id="cep">
+                        </div>
+                        <div class="input">
+                            <label for="senha">Senha: </label> <br>
+                            <input type="text" name="senha" id="senha">
+                        </div>
+                        <div class="input">
+                            <label for="rep_senha">Repita a senha: </label> <br>
+                            <input type="text" name="rep_senha" id="rep_senha">
+                        </div>
+                        <div class="input">
+                            <label for="datanasc">Data Nascimento: </label> <br>
+                            <input type="date" name="datanasc" id="datanasc">
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="login">
+                    <a href="login_2.html">Entrar</a>
+                </div>
+                <div class="botoes">
+                    <input type="submit" id="enviar" value="Enviar"> <br>
+                    <input type="reset" value="Limpar">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
